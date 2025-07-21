3fac8fc29d6a2a6e4990f363d25155693061007f
Prakhar_developer <prakhar_developer@outlook.com>
2025-07-21 06:56:11 +0530

    Initial commit1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // Fix for older MySQL versions (max key length issue)
+        Schema::defaultStringLength(191);
+
+        // Force HTTPS if REDIRECT_HTTPS is true in .env
         if (env('REDIRECT_HTTPS')) {
             URL::forceScheme('https');
         }
